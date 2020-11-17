a33f33cd0e71ee6b66ff4065a6a2551a40143480
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2020-11-17 07:27:57 +0000

    Add models

---
+++ Model/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Model
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int Locations { get; set; }
+        public int Count { get; set; }
+        public int Cities { get; set; }
+    }
+}
