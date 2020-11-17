526a9f071efcb8287adc396d00e397a9d3dd768c
Azeez Olusegun Odumosu <az6bcn@gmail.com>
2020-11-17 06:49:17 +0000

    project architecture

---
+++ Controllers/OpenAQDataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TechTask.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OpenAQDataController : ControllerBase
+    {
+        private readonly ILogger<OpenAQDataController> _logger;
+
+        public OpenAQDataController(ILogger<OpenAQDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
